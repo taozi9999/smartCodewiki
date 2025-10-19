a309a8bf3557a2108d03d1c57f9ee0bec677f4d3
Administrator <yhongtao38@gmail.com>
2025-10-20 01:11:53 +0800

    adfafaf

--- Interface/src/main/java/smartdeepwiki/CustomerApplication.java
+++ Interface/src/main/java/smartdeepwiki/CustomerApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  * 放在最外层（interface 层），负责启动 Spring 容器，
  * 自动扫描下层模块的 Bean（Application、Infrastructure）。
+ *
  */
 @SpringBootApplication(scanBasePackages = "smartdeepwiki")
 public class CustomerApplication {
